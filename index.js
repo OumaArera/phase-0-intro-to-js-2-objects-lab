7bc8dc1a07ba389cc5c0d679ac989c011bbfa8ca
OumaArera <johnouma999@gmail.com>
2023-12-14 13:58:31 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+const employee = {
+    name: "Rambungfee",
+    streetAddress: "Robert Ouko Street"
+}
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const updatedEmployee = {...employee};
+    updatedEmployee[key] = value;
+    return updatedEmployee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+
+    employee [key] = value;
+    return employee;
+  }
+  
+function deleteFromEmployeeByKey(employee, key) {
+
+    const newEmployee = Object.assign({}, employee);  
+
+    delete newEmployee[key];
+
+    return newEmployee;
+}  
+
+const destructivelyDeleteFromEmployeeByKey = (employee, key) => {
+    delete employee[key];
+    return employee;
+};
+
+  
